bc42cb8a5a8a43841c368e309a41645504743ea4
Ali <mohamedalisaifudeen1@yahoo.com>
2024-02-09 11:52:28 +0530

    first

---
+++ src/main/java/module-info.java
@@ -0,0 +1,11 @@
+module org.example.login {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires org.kordamp.bootstrapfx.core;
+
+    opens org.example.login to javafx.fxml;
+    exports org.example.login;
+}
